d0d13e55479bf662b90b5db929cb31798dbcf110
abhay9656 <abhay9656gupta@gmail.com>
2024-07-12 10:12:49 +0530

    Update list component with new labels and tasks

--- component/list.jsx
+++ component/list.jsx
@@ -24,36 +24,36 @@ const list = () => {
         <View style={{flex:1,flexDirection:'row',marginTop:15}}>
             <View style={styles.flex}>
                 <Icons name='clipboard' color={'blue'}  size={30}/>
-                <Text style={{fontSize:20,marginLeft:6,top:15}}>All</Text>
+                <Text style={{fontSize:20,marginLeft:6,top:15}}>Music</Text>
                 <Text style={{marginTop:10}}>23 task</Text>
             </View>
             <View style={[styles.flex,{marginLeft:10}]} >
                 <Ic name='bag-outline' color={'#e1ad01'} size={30}    />
-                <Text style={{fontSize:20,marginLeft:4,top:15}}>work</Text>
+                <Text style={{fontSize:20,marginLeft:4,top:15}}>Travel</Text>
                 <Text style={{marginTop:10}}>14 task</Text>
             </View>
         </View>
         <View style={{flex:1,flexDirection:'row',marginTop:15}}>
             <View style={styles.flex}>
                 <Icons name='clipboard' color={'blue'}  size={30}/>
-                <Text style={{fontSize:20,marginLeft:6,top:15}}>All</Text>
+                <Text style={{fontSize:20,marginLeft:6,top:15}}>Study</Text>
                 <Text style={{marginTop:10}}>23 task</Text>
             </View>
             <View style={[styles.flex,{marginLeft:10}]} >
                 <Ic name='bag-outline' color={'#e1ad01'} size={30}    />
-                <Text style={{fontSize:20,marginLeft:4,top:15}}>work</Text>
+                <Text style={{fontSize:20,marginLeft:4,top:15}}>Home</Text>
                 <Text style={{marginTop:10}}>14 task</Text>
             </View>
         </View>
         <View style={{flex:1,flexDirection:'row',marginTop:15}}>
             <View style={styles.flex}>
                 <Icons name='clipboard' color={'blue'}  size={30}/>
-                <Text style={{fontSize:20,marginLeft:6,top:15}}>All</Text>
+                <Text style={{fontSize:20,marginLeft:6,top:15}}>Game</Text>
                 <Text style={{marginTop:10}}>23 task</Text>
             </View>
             <View style={[styles.flex,{marginLeft:10}]} >
                 <Ic name='bag-outline' color={'#e1ad01'} size={30}    />
-                <Text style={{fontSize:20,marginLeft:4,top:15}}>work</Text>
+                <Text style={{fontSize:20,marginLeft:4,top:15}}>Shopping</Text>
                 <Text style={{marginTop:10}}>14 task</Text>
             </View>
         </View>
